4e00b11150b3d4861286d95ed04485b64e1c34c7
Pavel <barikovp@icloud.com>
2026-01-03 18:44:45 +0600

    feat: expandable descriptioin

--- src/components/TitleDescription.vue
+++ src/components/TitleDescription.vue
@@ -1,11 +1,30 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import { useMediaQuery } from "@vueuse/core";
+import ToggleButton from "./ToggleButton.vue";
+
 defineProps({
   text: String,
+});
+
+const isMobile = useMediaQuery("(max-width: 1209px)");
+const descriptionRef = ref(null);
+const isExpanded = ref(false)
+
+onMounted(() => {
+  if (descriptionRef.value.scrollHeight <= descriptionRef.value.clientHeight) {
+    isExpanded.value = true;
+  }
 });
 </script>
 
 <template>
-  <div v-html="text" />
+  <div :class="['description', { expanded: isExpanded }]" ref="descriptionRef">
+    <div v-html="text" />
+    <div v-if="!isExpanded && !isMobile" class="shadow">
+      <ToggleButton v-model="isExpanded">Read more</ToggleButton>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -13,13 +32,33 @@ p {
   margin: 0;
 }
 
-div {
+.description {
   width: 310px;
 }
 
 @media screen and (min-width: 1210px) {
-  div {
+  .description {
     width: 100%;
+    height: 1px;
+    flex: 1 1 auto;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .shadow {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100px;
+    background: linear-gradient(to top, var(--neko-theme-bg-color) 0%, var(--neko-theme-shadow-gradient-color) 100%);
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+  }
+
+  .expanded {
+    height: auto;
   }
 }
 </style>
---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+const isActive = defineModel()
+</script>
+
+<template>
+  <button @click="isActive = !isActive">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+button {
+  border: none;
+  outline: none;
+  background: none;
+  color: var(--neko-theme-text-color);
+  font-family: "Geist", sans-serif;
+  font-size: 16px;
+  padding: 0;
+  margin: 5px;
+  cursor: pointer;
+}
+</style>
